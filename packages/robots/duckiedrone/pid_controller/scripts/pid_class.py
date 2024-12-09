040e7a81c81dba736155ad60c941a4cfd18fb89c
Davide Iafrate <dvde.iafrate98@gmail.com>
2024-12-09 12:44:01 +0000

    Refactor PID controller: Improve parameter handling, enhance integral calculations, and add xy_plane_cmd_to_rc_cmd method for RC command mapping.

--- packages/robots/duckiedrone/pid_controller/scripts/pid_class.py
+++ packages/robots/duckiedrone/pid_controller/scripts/pid_class.py
@@ -1,39 +1,42 @@
 #!/usr/bin/env python3
 
+from cmath import inf
 import logging
 from typing import List, Optional
+
+import numpy as np
 import rospy
 
 
-#####################################################
-#						PID							#
-#####################################################
 class PIDaxis:
 
     def __init__(self,
                  kp, ki, kd,
-                 i_range=None,
+                 i_range=(1000,2000),
                  d_range=None,
                  control_range=(1000, 2000),
                  midpoint=1500,
                  smoothing=True):
-        # Tuning
+        
         self.kp = kp
         self.ki = ki
         self.kd = kd
-        # Config
+        
         self.i_range = i_range
         self.d_range = d_range
         self.control_range = control_range
+        assert control_range[0] < midpoint < control_range[1]
+    
         self.midpoint = midpoint
         self.smoothing = smoothing
-        # initial i value
+        
         self.init_i = 0.0
 
-        # Internal
+        # Internal variables
         self._old_err = None
         self._p = 0
         self.integral = self.init_i
+        
         # effective only once
         self.init_i = 0.0
         self._d = 0
@@ -51,22 +54,22 @@ class PIDaxis:
         self._ddd = 0
 
     def step(self, err, time_elapsed) -> int:
-        if time_elapsed == 0:
+        if time_elapsed <= 0:
             return 0
 
         if self._old_err is None:
             # First time around prevent d term spike
             self._old_err = err
 
-        # Find the p component
+        # Compute the p component
         self._p = err * self.kp
 
-        # Find the i component
+        # Compute the i component
         self.integral += err * self.ki * time_elapsed
         if self.i_range is not None:
-            self.integral = max(self.i_range[0], min(self.integral, self.i_range[1]))
+            self.integral = np.clip(self.integral, self.i_range[0], self.i_range[1])
 
-        # Find the d component
+        # Compute the d component
         self._d = (err - self._old_err) * self.kd / time_elapsed
         if self.d_range is not None:
             self._d = max(self.d_range[0], min(self._d, self.d_range[1]))
@@ -80,30 +83,27 @@ class PIDaxis:
 
         # Calculate control output
         raw_output = self._p + self.integral + self._d
-        output = min(max(raw_output + self.midpoint, self.control_range[0]), self.control_range[1])
+        output = np.clip(raw_output + self.midpoint, self.control_range[0], self.control_range[1])
 
         return int(output)
 
 
 # noinspection DuplicatedCode
 class PID:
-    height_factor = 1.238
-    battery_factor = 0.75
 
     def __init__(self,
-                 roll=PIDaxis(
+                roll=PIDaxis(
                      4.0, 1.0, 0.0,
-                     control_range=(1400, 1600),
-                     midpoint=1500,
+                     control_range=(-inf, +inf),
+                     midpoint=0,
                      i_range=(-100, 100)
                  ),
                  roll_low=PIDaxis(
-                     0.0, 0.5, 0.0,
-                     control_range=(1400, 1600),
-                     midpoint=1500,
+                     0.0, 0.0, 0.0,
+                     control_range=(-inf, +inf),
+                     midpoint=0,
                      i_range=(-150, 150)
                  ),
-
                  pitch=PIDaxis(
                      4.0, 1.0, 0.0,
                      control_range=(1400, 1600),
@@ -119,18 +119,14 @@ class PID:
 
                  yaw=PIDaxis(0.0, 0.0, 0.0),
 
-                 # Kv 2300 motors have midpoint 1300, Kv 2550 motors have midpoint 1250
-                 # height_safety_here (in the sense that the motors are limited)
-                 # 1.0, 0.5, 2.0
-                 # 1.0, 0.05, 2.0
                  throttle=PIDaxis(
-                     5.0 / height_factor * battery_factor,
-                     0.5 / height_factor * battery_factor,
-                     10.0 / height_factor * battery_factor,
-                     i_range=(-400, 400),
-                     control_range=(1200, 1500),
-                     d_range=(-40, 40),
-                     midpoint=1500
+                     kp=30.0,
+                     ki=15.0,
+                     kd=10.0,
+                     i_range=(-100, 100),
+                     control_range=(1200, 1600),
+                     d_range=(-100, 100),
+                     midpoint=1500,
                  ),
                  ):
         self.trim_controller_cap_plane = 0.05
@@ -163,11 +159,8 @@ class PID:
         self._t = None
 
         # reset individual PIDs
-        self.roll.reset()
-        self.roll_low.reset()
-        self.pitch.reset()
-        self.pitch_low.reset()
-        self.throttle.reset()
+        for pid in [self.roll, self.roll_low, self.pitch, self.pitch_low, self.throttle]:
+            pid.reset()
 
     def step(self, error, cmd_yaw_velocity=0) -> List[int]:
         """ Compute the control variables from the error using the step methods
@@ -181,10 +174,7 @@ class PID:
 
         self._t = rospy.get_time()
 
-        # Compute roll command
-        ######################
-        # if the x velocity error is within the threshold
-        cmd_r = self.compute_axis_command(
+        cmd_roll = self.compute_axis_command(
             error.x,
             time_elapsed,
             pid_low=self.roll_low,
@@ -192,25 +182,30 @@ class PID:
             trim_controller=self.trim_controller_cap_plane
             )
 
-        # Compute pitch command
-        cmd_p = self.compute_axis_command(
+        cmd_pitch = self.compute_axis_command(
             error.y,
             time_elapsed,
             pid_low=self.pitch_low,
             pid=self.pitch,
             trim_controller=self.trim_controller_cap_plane
             )
-        # Compute yaw command
-        cmd_y = 1500 + cmd_yaw_velocity
 
-        cmd_t = self.compute_axis_command(
+        cmd_yaw = 1500 + cmd_yaw_velocity
+
+        cmd_throttle = self.compute_axis_command(
             error.z,
             time_elapsed,
             pid_low=None,
             pid=self.throttle,
             trim_controller=self.trim_controller_cap_throttle
         )
-        return [cmd_r, cmd_p, cmd_y, cmd_t]
+
+        return [
+            self.xy_plane_cmd_to_rc_cmd(np.degrees(cmd_roll)),
+            self.xy_plane_cmd_to_rc_cmd(np.degrees(cmd_pitch)),
+            cmd_yaw,
+            cmd_throttle,
+        ]
 
     def compute_axis_command(
         self,
@@ -221,15 +216,15 @@ class PID:
         trim_controller: float = 5,
     ) -> int:
         if pid_low is None:
-            cmd_r = pid.step(error, time_elapsed)
-            return cmd_r
+            cmd = pid.step(error, time_elapsed)
+            return cmd
 
         if abs(error) < self.trim_controller_thresh_plane:
             # pass the high rate i term off to the low rate pid
             pid_low.integral += pid.integral
             pid.integral = 0
             # set the roll value to just the output of the low rate pid
-            cmd_r = pid_low.step(error, time_elapsed)
+            cmd = pid_low.step(error, time_elapsed)
         else:
             if error > trim_controller:
                 pid_low.step(trim_controller, time_elapsed)
@@ -237,6 +232,41 @@ class PID:
                 pid_low.step(-trim_controller, time_elapsed)
             else:
                 pid_low.step(error, time_elapsed)
-            cmd_r = pid_low.integral + pid.step(error, time_elapsed)
+            cmd = pid_low.integral + pid.step(error, time_elapsed)
         
-        return cmd_r
+        return cmd
+
+    @staticmethod
+    def xy_plane_cmd_to_rc_cmd(
+        commanded_angle: float,
+        rc_max: int = 1900,
+        rc_min: int = 1100,
+        rc_midpoint: int = 1500,
+        max_allowed_angle: float = 30,
+    ) -> int:
+        """
+        Map the commanded angle to a roll and pitch RC command according to the Ardupilot convention.
+        The RC command value is calculated using the following formula:
+
+            rc_command = rc_midpoint + (commanded_angle/max_allowed_angle) * (rc_max - rc_min)
+
+        .. math::
+
+            \text{RC command} = \text{rc_midpoint} + \left( \frac{\text{cmd}}{\text{max_allowed_angle}} \right) \times (\text{rc_max} - \text{rc_min})
+
+
+        Args:
+            cmd (float): The commanded angle in degrees.
+            rc_max (int, optional): The maximum RC command value. Defaults to 1900.
+            rc_min (int, optional): The minimum RC command value. Defaults to 1100.
+            rc_midpoint (int, optional): The midpoint RC command value. Defaults to 1500.
+            theta_max (float, optional): The maximum angle in degrees. Defaults to 30.
+
+        Returns:
+            int: The corresponding RC command value.
+
+        """
+
+        return int(
+            commanded_angle / max_allowed_angle * (rc_max - rc_min) + rc_midpoint
+        )
